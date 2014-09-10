582ab13970c79d67cae8229478a6253eadcf0788
Jan Monschke <jan.monschke@gmail.com>
2014-09-10 18:34:15 +0200

    work on demo

--- tgb-demo/index.js
+++ tgb-demo/index.js
@@ -6,20 +6,34 @@ nx.onload = function(){
   var ksch = BufferHandler.load('audio/kschhh.mp3').done(function(id){ ksch = id;});
   var gogo = BufferHandler.load('audio/gogogogo.mp3').done(function(id){ gogo = id;});
   var dommmdoo = BufferHandler.load('audio/dommmdoo.mp3').done(function(id){ dommmdoo = id;});
-
+  var wowow = BufferHandler.load('audio/wowwowwow-2.mp3').done(function(id){ wowow = id;});
 
 
 
   $.when.apply($, [background, ksch, gogo, dommmdoo]).done(function(){
-    matrix1.row = 2;
-    matrix1.draw();
-    matrix1.col = 4;
-    matrix1.draw();
+    // matrix1.row = 2;
+    // matrix1.draw();
+    // matrix1.col = 4;
+    // matrix1.draw();
     button1.response = function(data){
-      if(data.press)
+      if(data.press){
+        BufferHandler.stop(dommmdoo);
         BufferHandler.play(dommmdoo);
-      console.log(data)
+      }
     }
+
+    button2.response = function(data){
+      if(data.press){
+        BufferHandler.stop(wowow);
+        BufferHandler.play(wowow);
+      }
+    }
+
+    button3.response = function(data){
+      if(data.press)
+        BufferHandler.play(ksch);
+    }
+
     //
     // BufferHandler.play(gogo);
     // BufferHandler.play(dommmdoo);
