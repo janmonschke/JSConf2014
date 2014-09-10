b1f3d771900df4435cb7c3c902e1481085f51263
Jan Monschke <jan.monschke@gmail.com>
2014-09-10 16:54:10 +0200

    demo progress

---
+++ tgb-demo/index.js
@@ -0,0 +1,12 @@
+var background = BufferHandler.load('audio/background.mp3').done(function(id){ background = id;});
+var ksch = BufferHandler.load('audio/kschhh.mp3').done(function(id){ ksch = id;});
+var gogo = BufferHandler.load('audio/gogogogo.mp3').done(function(id){ gogo = id;});
+var dommmdoo = BufferHandler.load('audio/dommmdoo.mp3').done(function(id){ dommmdoo = id;});
+
+
+$.when.apply($, [background, ksch, gogo, dommmdoo]).done(function(){
+  console.log('done')
+  BufferHandler.play(ksch);
+  BufferHandler.play(gogo);
+  BufferHandler.play(dommmdoo);
+});
