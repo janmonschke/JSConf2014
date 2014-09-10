43346e0d73300feb6bc321be496b4c311fd32f77
Jan Monschke <jan.monschke@gmail.com>
2014-09-10 17:03:52 +0200

    start on load

--- tgb-demo/index.js
+++ tgb-demo/index.js
@@ -1,14 +1,16 @@
-var background = BufferHandler.load('audio/background.mp3').done(function(id){ background = id;});
-var ksch = BufferHandler.load('audio/kschhh.mp3').done(function(id){ ksch = id;});
-var gogo = BufferHandler.load('audio/gogogogo.mp3').done(function(id){ gogo = id;});
-var dommmdoo = BufferHandler.load('audio/dommmdoo.mp3').done(function(id){ dommmdoo = id;});
+window.onload = function(){
+  var background = BufferHandler.load('audio/background.mp3').done(function(id){ background = id;});
+  var ksch = BufferHandler.load('audio/kschhh.mp3').done(function(id){ ksch = id;});
+  var gogo = BufferHandler.load('audio/gogogogo.mp3').done(function(id){ gogo = id;});
+  var dommmdoo = BufferHandler.load('audio/dommmdoo.mp3').done(function(id){ dommmdoo = id;});
 
 
-$.when.apply($, [background, ksch, gogo, dommmdoo]).done(function(){
-  console.log('done')
-  BufferHandler.play(ksch);
-  BufferHandler.play(gogo);
-  BufferHandler.play(dommmdoo);
+  $.when.apply($, [background, ksch, gogo, dommmdoo]).done(function(){
+    console.log('done')
+    BufferHandler.play(ksch);
+    BufferHandler.play(gogo);
+    BufferHandler.play(dommmdoo);
 
 
-});
+  });
+}
